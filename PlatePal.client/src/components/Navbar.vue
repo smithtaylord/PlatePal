f64440e2793761681e45e97afa1b8b64cf27c545
Taylor Smith <smith.taylord@gmail.com>
2023-03-23 14:55:09 -0600

    Recipes drawing to screen, a little bit has been done to the navbar

--- PlatePal.client/src/components/Navbar.vue
+++ PlatePal.client/src/components/Navbar.vue
@@ -1,32 +1,25 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+  <nav class="bg-dark p-3 mb-3">
+    <div class="text-end">
+      <Login />
+    </div>
+    <router-link class=" text-center" :to="{ name: 'Home' }">
+      <div class="">
+        <h1 class="text-light text-center">PlatePal <i class="mdi mdi-shaker-outline"></i></h1>
+        <div class="text-light fs-4">Your recipe companion for every meal</div>
       </div>
     </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
+
         </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
-    </div>
+      </ul> -->
+    <!-- LOGIN COMPONENT HERE -->
   </nav>
 </template>
 
@@ -40,25 +33,4 @@ export default {
 }
 </script>
 
-<style scoped>
-a:hover {
-  text-decoration: none;
-}
-
-.nav-link {
-  text-transform: uppercase;
-}
-
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
-}
-
-</style>
+<style scoped></style>
--- PlatePal.client/src/pages/HomePage.vue
+++ PlatePal.client/src/pages/HomePage.vue
@@ -1,44 +1,46 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-center" v-for="r in recipes">
+        <div class="p-3 mt-4">
+          <img class="recipe-img" :src="r.img" :alt="r.title">
+          <div>{{ r.title }}</div>
+          <div>{{ r.category }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { onMounted, computed } from 'vue';
+import { recipesService } from '../services/RecipesService.js'
+import Pop from '../utils/Pop.js';
+import { AppState } from '../AppState.js';
+
 export default {
   setup() {
-    return {}
+    async function getAllRecipes() {
+      try {
+        await recipesService.getALlRecipes();
+      } catch (error) {
+        Pop.error(error, "[error getting recipes]")
+      }
+    }
+    onMounted(() => {
+      getAllRecipes();
+    })
+    return {
+      recipes: computed(() => AppState.recipes)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.recipe-img {
+  height: 35vh;
+  width: 35vh;
+  object-fit: cover;
 }
 </style>
